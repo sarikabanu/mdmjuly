2697930c237929868669b1f1ce384537bf5af845
sarikab <sarika.b@shrigowri.com>
2018-07-16 12:38:10 +0530

    port

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ _Routes.app.configure(app);
 _Routes.device.configure(app);
 
 
-var server = app.listen(process.env.PORT || 6000, function() {
+var server = app.listen(process.env.PORT || 7000, function() {
     console.log('Server listening on port ' + server.address().port);
 });
